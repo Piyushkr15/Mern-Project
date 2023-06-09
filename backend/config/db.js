8a0fe9242faeb41983a42381acf8d82f5958fd56
Piyush Kumar Anand <piyush@192.168.1.6>
2023-06-09 15:04:03 +0530

    Deploying commands

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async() => {
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error(`Erro: ${error.message}`);
+        console.error(`Error: ${error.message}`);
     }
 
 }
--- backend/server.js
+++ backend/server.js
@@ -17,13 +17,13 @@ app.use("/api/users", userRoutes)
 app.use("/api/notes", noteRoutes)
 
 // --------------------------deployment------------------------------
-__dirname = path.resolve();
-
+// __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    // res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
   );
 } else {
   app.get("/", (req, res) => {
