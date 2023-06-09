f3588f0c0199eee49af2782efa8ca955d498ce70
Piyush Kumar Anand <piyush@192.168.1.6>
2023-06-09 15:47:19 +0530

    final commit

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
         >
             <Container>
                 <Row>
-                    <Col className="text-center py-3">Copyright &copy; Cooldude69</Col>
+                    <Col className="text-center py-3">Copyright &copy; Piyush</Col>
                 </Row>
             </Container>
         </footer>
